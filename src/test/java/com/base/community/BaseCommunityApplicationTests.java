5f475a66a67da1c86b51a38535593d2de40e051e
soheeKang <thgml352@gmail.com>
2022-10-15 01:31:15 +0900

    feat: spring boot 초기화 설정

---
+++ src/test/java/com/base/community/BaseCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
