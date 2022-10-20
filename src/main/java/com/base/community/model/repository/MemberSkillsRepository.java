cba641720c4162367b2771932748edf279113aef
Park <zalop3128@kakao.com>
2022-10-21 00:23:10 +0900

    FEAT : 회원정보 스킬 삭제하기

--- src/main/java/com/base/community/model/repository/MemberSkillsRepository.java
+++ src/main/java/com/base/community/model/repository/MemberSkillsRepository.java
@@ -4,5 +4,9 @@ package com.base.community.model.repository;
 import com.base.community.model.entity.MemberSkills;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberSkillsRepository extends JpaRepository<MemberSkills, Long> {
+
+    Optional<MemberSkills> findByMemberIdAndId(Long memberId, Long skillId);
 }
