d3b3e6ac897293d322e86ecc89c43579244adfe3
Park <zalop3128@kakao.com>
2022-10-28 08:41:48 +0900

    REFACTOR : 로그인 및 마이페이지 수정

---
+++ src/main/java/com/base/community/dto/UpdateMemberDto.java
@@ -0,0 +1,19 @@
+package com.base.community.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateMemberDto {
+    private Long id;
+    private String Nickname;
+    private LocalDate birth;
+    private String phone;
+    private List<MemberSkillsDto> skills;
+}
