41ab57826e4a4f070891f88f23df11bf01e6a6d4
soheeKang <thgml352@gmail.com>
2022-10-28 22:12:56 +0900

    feat: 프로젝트 신청/수락 기능

---
+++ src/main/java/com/base/community/model/repository/ProjectMemberRepository.java
@@ -0,0 +1,9 @@
+package com.base.community.model.repository;
+
+import com.base.community.model.entity.ProjectMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
+}
