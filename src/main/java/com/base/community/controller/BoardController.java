8c04a15d6993e97d8dc261c21c598109e3afd9c3
Park <zalop3128@kakao.com>
2022-11-09 02:12:37 +0900

    REFACTOR : 게시판 검색 카테고리, 검색키워드 처리

--- src/main/java/com/base/community/controller/BoardController.java
+++ src/main/java/com/base/community/controller/BoardController.java
@@ -7,6 +7,7 @@ import com.base.community.dto.BoardDto;
 import com.base.community.security.TokenProvider;
 import com.base.community.service.BoardService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/boards")
+@Slf4j
 public class BoardController {
     private BoardService boardService;
     private final TokenProvider tokenProvider;
@@ -21,39 +23,39 @@ public class BoardController {
     // 게시글 전체보기
     @GetMapping()
     public ResponseEntity<?> boardList(@RequestParam(required = false) String category,
-                                          @RequestParam(defaultValue = "1") int page){
+                                       @RequestParam(required = false) String keyword,
+                                       @RequestParam(defaultValue = "1") int page) {
+        log.info("category : " + category + "| keyword : " + keyword);
+        return ResponseEntity.ok(boardService.boardList(category, keyword, page - 1));
+    }
 
-         return ResponseEntity.ok(boardService.boardList(category, page-1));
-   }
-
-   // 게시글 상세보기
     @GetMapping("/{boardId}")
-    public ResponseEntity<BoardDetailDto> boardDetail(@PathVariable Long boardId){
+    public ResponseEntity<BoardDetailDto> boardDetail(@PathVariable Long boardId) {
 
         return ResponseEntity.ok(boardService.boardDetail(boardId));
     }
 
-   // 내가 작성한 게시글 목록 보기
+    // 내가 작성한 게시글 목록 보기
     @GetMapping("/myBoardList")
-    public  ResponseEntity<?> myBoardList(@RequestHeader(name = "X-AUTH-TOKEN") String token,
-                                    @RequestParam(defaultValue = "1") int page){
+    public ResponseEntity<?> myBoardList(@RequestHeader(name = "X-AUTH-TOKEN") String token,
+                                         @RequestParam(defaultValue = "1") int page) {
 
-        return ResponseEntity.ok(boardService.myBoardList(tokenProvider.getUser(token).getId(), page-1));
+        return ResponseEntity.ok(boardService.myBoardList(tokenProvider.getUser(token).getId(), page - 1));
     }
 
     // 내가 좋아요한 게시글 목록 보기
     @GetMapping("/myHeartList")
     public ResponseEntity<?> myHeartList(@RequestHeader(name = "X-AUTH-TOKEN") String token,
-                                      @RequestParam(defaultValue = "1") int page){
+                                         @RequestParam(defaultValue = "1") int page) {
 
-        return ResponseEntity.ok(boardService.myHeartList(tokenProvider.getUser(token).getId(), page-1));
+        return ResponseEntity.ok(boardService.myHeartList(tokenProvider.getUser(token).getId(), page - 1));
     }
 
 
     // 게시글 작성
     @PostMapping()
     public ResponseEntity<Long> write(@RequestHeader(name = "X-AUTH-TOKEN") String token
-    , @RequestBody BoardDto form){
+            , @RequestBody BoardDto form) {
 
         return ResponseEntity.ok(boardService.writeBoard(form, tokenProvider.getUser(token).getId()));
     }
@@ -61,15 +63,15 @@ public class BoardController {
     //게시글 수정
     @PutMapping("/{boardId}")
     public ResponseEntity<Long> modify(@RequestHeader(name = "X-AUTH-TOKEN") String token
-            , @RequestBody BoardDto form, @PathVariable Long boardId){
+            , @RequestBody BoardDto form, @PathVariable Long boardId) {
 
-        return ResponseEntity.ok(boardService.modifyBoard(form, tokenProvider.getUser(token).getId(),boardId));
+        return ResponseEntity.ok(boardService.modifyBoard(form, tokenProvider.getUser(token).getId(), boardId));
     }
 
     //게시글 삭제
     @DeleteMapping("/{boardId}")
     public ResponseEntity<String> delete(@RequestHeader(name = "X-AUTH-TOKEN") String token
-            , @PathVariable Long boardId){
+            , @PathVariable Long boardId) {
 
         return ResponseEntity.ok(boardService.deleteBoard(tokenProvider.getUser(token).getId(), boardId));
     }
@@ -77,7 +79,7 @@ public class BoardController {
     // 댓글 작성
     @PostMapping("/{boardId}/comments")
     public ResponseEntity<Long> writeComment(@RequestHeader(name = "X-AUTH-TOKEN") String token,
-                                             @RequestBody BoardCommentDto form, @PathVariable Long boardId){
+                                             @RequestBody BoardCommentDto form, @PathVariable Long boardId) {
 
         return ResponseEntity.ok(boardService.writeBoardComment(form, tokenProvider.getUser(token).getId(), boardId));
     }
@@ -85,25 +87,25 @@ public class BoardController {
     // 댓글 수정
     @PutMapping("/comments/{commentId}")
     public ResponseEntity<Long> modifyComment(@RequestHeader(name = "X-AUTH-TOKEN") String token,
-                                              @RequestBody BoardCommentDto form, @PathVariable Long commentId){
-        return ResponseEntity.ok(boardService.modifyComment(form, tokenProvider.getUser(token).getId() ,commentId));
+                                              @RequestBody BoardCommentDto form, @PathVariable Long commentId) {
+        return ResponseEntity.ok(boardService.modifyComment(form, tokenProvider.getUser(token).getId(), commentId));
     }
 
     // 댓글 삭제
     @DeleteMapping("/comments/{commentId}")
     public ResponseEntity<String> deleteComment(@RequestHeader(name = "X-AUTH-TOKEN") String token
-            , @PathVariable Long commentId){
+            , @PathVariable Long commentId) {
 
-        return ResponseEntity.ok(boardService.deleteComment(tokenProvider.getUser(token).getId() , commentId));
+        return ResponseEntity.ok(boardService.deleteComment(tokenProvider.getUser(token).getId(), commentId));
 
     }
 
 
     // 좋아요 | 좋아요 취소
     @GetMapping("/{boardId}/hearts")
-    public ResponseEntity<Boolean> heart(@PathVariable Long boardId , @RequestHeader(name = "X-AUTH-TOKEN") String token){
+    public ResponseEntity<Boolean> heart(@PathVariable Long boardId, @RequestHeader(name = "X-AUTH-TOKEN") String token) {
 
-        return  ResponseEntity.ok(boardService.heart(tokenProvider.getUser(token).getId(), boardId));
+        return ResponseEntity.ok(boardService.heart(tokenProvider.getUser(token).getId(), boardId));
     }
 
 
