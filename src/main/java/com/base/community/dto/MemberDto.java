11d553216d4311cee86f1f804b71f1e8d912b392
soheeKang <thgml352@gmail.com>
2022-11-22 20:20:25 +0900

    close #76 MemberService response type 수정

--- src/main/java/com/base/community/dto/MemberDto.java
+++ src/main/java/com/base/community/dto/MemberDto.java
@@ -3,22 +3,19 @@ package com.base.community.dto;
 
 import com.base.community.model.entity.Member;
 import com.base.community.model.entity.MemberSkills;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class MemberDto {
     private Long id;
     private String email;
-    private String password;
     private String name;
     private String nickname;
     private LocalDate birth;
@@ -26,7 +23,5 @@ public class MemberDto {
     private String filename; //파일이름
     private String urlFilename;  //파일주소
     private List<MemberSkillsDto> skills;
-
-
 
 }
